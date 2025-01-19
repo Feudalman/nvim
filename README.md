3160fd16e7b7604f7cb64a825be7a90527007904
Zirui Cai <zirui.cai@swanhub.co>
2025-01-19 16:15:56 +0800

    feat: format

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ nvim 相关配置
 
 > 可以参考
 > https://martinlwx.github.io/zh-cn/config-neovim-from-scratch/
+
+克隆本项目后，移动到 `~/.config/nvim/` 下，确保目录名为 `nvim`。
+
+并安装 `lua`：`https://www.lua.org/download.html`。
+
+> 后续再补充详细配置解析
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,11 +1,12 @@
 -- 通过 lazy 导入插件
 require("lazy").setup({
-	-- LSP manager
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
-  "mhartington/formatter.nvim",
+-- LSP manager
+"williamboman/mason.nvim",
+"williamboman/mason-lspconfig.nvim",
+"neovim/nvim-lspconfig",
 
+-- 格式化
+"mhartington/formatter.nvim",
 
 -- 主题
 {
@@ -17,43 +18,43 @@ require("lazy").setup({
 
 -- markdown
 {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
 },
 
 -- 文件树
 {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    filesystem = {
-      filtered_items = {
-        visible = true, -- 允许显示隐藏文件和目录
-        hide_dotfiles = false, -- 显示 . 开头的项目
-        hide_gitignored = false, -- 显示被 .gitignore 忽略的文件
-      }
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  filesystem = {
+    filtered_items = {
+      visible = true, -- 允许显示隐藏文件和目录
+      hide_dotfiles = false, -- 显示 . 开头的项目
+      hide_gitignored = false, -- 显示被 .gitignore 忽略的文件
     }
+  }
 },
 
 -- 切换窗口
 {
-    's1n7ax/nvim-window-picker',
-    name = 'window-picker',
-    event = 'VeryLazy',
-    version = '2.*',
-    config = function()
-        require'window-picker'.setup()
-    end,
+  's1n7ax/nvim-window-picker',
+  name = 'window-picker',
+  event = 'VeryLazy',
+  version = '2.*',
+  config = function()
+  require'window-picker'.setup()
+  end,
 },
 
 -- 允许图片插入到md等标记型文件
@@ -70,42 +71,43 @@ require("lazy").setup({
   },
 },
 
--- codeium
-  {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-    },
-    config = function()
-        require("codeium").setup({
-        })
-    end
+-- 使用 codeium 进行 ai 补全
+{
+  "Exafunction/codeium.nvim",
+  dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
   },
+  config = function()
+      require("codeium").setup({
+      })
+  end
+},
 
 
 -- vscode 风格图标
-	{
-		"onsails/lspkind.nvim",
-		event = { "VimEnter" },
+{
+  "onsails/lspkind.nvim",
+	event = { "VimEnter" },
+},
+-- 自动补全插件
+{
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"lspkind.nvim",
+		"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+		"hrsh7th/cmp-buffer", -- buffer auto-completion
+  	"hrsh7th/cmp-path", -- path auto-completion
+		"hrsh7th/cmp-cmdline", -- cmdline auto-completion
 	},
-	-- 自动补全插件
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"lspkind.nvim",
-			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
-			"hrsh7th/cmp-buffer", -- buffer auto-completion
-			"hrsh7th/cmp-path", -- path auto-completion
-			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
-		},
-		config = function()
-			require("config.nvim-cmp")
-		end,
-	},
-	-- 代码片段引擎
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-	},
+	config = function()
+	require("config.nvim-cmp")
+	end,
+},
+-- 代码片段引擎
+{
+	"L3MON4D3/LuaSnip",
+	version = "v2.*",
+},
+
 })
